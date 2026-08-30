6a0dea7094ede60474681d655ea71f282d433dba
Sohail Elskhawy <sohailelskhawy2@gmail.com>
2026-08-30 10:11:17 +0300

    feat(web): adopt lucide-react for standardized iconography and clean icons adapter

---
+++ apps/web/src/components/icons.tsx
@@ -0,0 +1,79 @@
+import {
+  AlertTriangle as LucideAlertTriangle,
+  ArrowLeft as LucideArrowLeft,
+  ArrowRight as LucideArrowRight,
+  BarChart3 as LucideBarChart3,
+  Briefcase as LucideBriefcase,
+  Check as LucideCheck,
+  ChevronDown as LucideChevronDown,
+  ClipboardList as LucideClipboardList,
+  Clock as LucideClock,
+  FileText as LucideFileText,
+  Flag as LucideFlag,
+  Handshake as LucideHandshake,
+  Lightbulb as LucideLightbulb,
+  Menu as LucideMenu,
+  MessageCircleWarning as LucideMessageCircleWarning,
+  MessageSquare as LucideMessageSquare,
+  MessageSquareQuote as LucideMessageSquareQuote,
+  MessagesSquare as LucideMessagesSquare,
+  Mic as LucideMic,
+  Play as LucidePlay,
+  RotateCw as LucideRotateCw,
+  Search as LucideSearch,
+  Send as LucideSend,
+  ShieldAlert as LucideShieldAlert,
+  ShieldCheck as LucideShieldCheck,
+  Sparkles as LucideSparkles,
+  Star as LucideStar,
+  Target as LucideTarget,
+  Trash2 as LucideTrash2,
+  TrendingUp as LucideTrendingUp,
+  User as LucideUser,
+  X as LucideX,
+  type LucideProps,
+} from "lucide-react";
+
+export type IconProps = LucideProps;
+
+// Navigation & Structure
+export const ArrowRightIcon = LucideArrowRight;
+export const ArrowLeftIcon = LucideArrowLeft;
+export const ChevronDownIcon = LucideChevronDown;
+export const MenuIcon = LucideMenu;
+
+// Actions & States
+export const CheckIcon = LucideCheck;
+export const CloseIcon = LucideX;
+export const TrashIcon = LucideTrash2;
+export const RefreshIcon = LucideRotateCw;
+export const SearchIcon = LucideSearch;
+export const SendIcon = LucideSend;
+export const PlayIcon = LucidePlay;
+export const FlagIcon = LucideFlag;
+export const MicIcon = LucideMic;
+
+// Status & Feedback
+export const AlertTriangleIcon = LucideAlertTriangle;
+export const ShieldCheckIcon = LucideShieldCheck;
+export const TargetIcon = LucideTarget;
+export const StarIcon = LucideStar;
+export const SparklesIcon = LucideSparkles;
+export const LightbulbIcon = LucideLightbulb;
+export const TimerIcon = LucideClock;
+
+// Content & Dialogue
+export const ForumIcon = LucideMessageSquare;
+export const DocumentTextIcon = LucideFileText;
+export const AssignmentIcon = LucideClipboardList;
+export const UserIcon = LucideUser;
+export const SpeakingWithIcon = LucideMessagesSquare;
+export const BarChartIcon = LucideBarChart3;
+
+// Scenario Categories
+export const HandshakeIcon = LucideHandshake;
+export const InterviewIcon = LucideBriefcase;
+export const PromotionIcon = LucideTrendingUp;
+export const PushbackIcon = LucideMessageCircleWarning;
+export const FeedbackIcon = LucideMessageSquareQuote;
+export const BoundaryIcon = LucideShieldAlert;
